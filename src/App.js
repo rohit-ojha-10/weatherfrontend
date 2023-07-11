6fe0f90ee4fe5deffe036b44ce17f48f20b3508a
rohit-ojha-10 <yorohit.2002@gmail.com>
2023-07-12 01:16:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  let city1 = useRef();
+  const city2 = useRef();
+  const city3 = useRef();
+  const submitted = useRef();
+  const [data,setData] = useState();
+  const headers = {
+    'Content-type':'application/json'
+  }
+  let cities = [];
+ 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    cities.push(city1.current.value)
+    cities.push(city2.current.value)
+    cities.push(city3.current.value)
+    console.log(cities);
+    await axios.post('https://weatherbackend-3err.onrender.com/weatherupdate',{
+      cities:cities
+    })
+    .then((resp) => {
+      // console.log(resp.data);
+      submitted.current = "";
+      setData(resp.data)
+      console.log(data)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form onSubmit={ (e) => handleSubmit(e)}>
+      <Form.Group  className="mb-3" controlId="formBasicEmail">
+        <Form.Label>First City</Form.Label>
+        <Form.Control ref={city1} type="text" placeholder="" />
+      </Form.Group>
+      <Form.Group  className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Second City</Form.Label>
+        <Form.Control ref={city2} type="text" />
+      </Form.Group>
+      <Form.Group  className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Third City</Form.Label>
+        <Form.Control ref={city3} type="text" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    <p class="display-4" >Results will load below in a second after submitting , please write the correct spelling of the cities</p>
+    <div>{data && Object.keys(data.weather).map((city,idx) => {
+      return <h2 class="display-2" key={idx}>{`${city}'s weather = ${data.weather[city]}`}</h2>
+    })}</div>
     </div>
   );
 }
